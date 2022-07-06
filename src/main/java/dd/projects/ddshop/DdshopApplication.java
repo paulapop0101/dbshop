c9055cac91db4f22b204243eefc9355366611cad
ddshop <poppaula0106@gmail.com>
2022-07-06 15:33:52 +0300

    base

---
+++ src/main/java/dd/projects/ddshop/DdshopApplication.java
@@ -0,0 +1,13 @@
+package dd.projects.ddshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdshopApplication.class, args);
+	}
+
+}
